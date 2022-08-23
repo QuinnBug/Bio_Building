7c701bfc1faceee57eb5957c54fb1b4745c9e96f
Charlie Blaise <charlie.h.blaise@gmail.com>
2022-08-23 13:53:12 +0100

    Changed main menu scene and added animations

--- Assets/Scripts/LoginScripts/LoginCanvasController.cs
+++ Assets/Scripts/LoginScripts/LoginCanvasController.cs
@@ -9,6 +9,7 @@ public class LoginCanvasController : MonoBehaviour
     public static LoginCanvasController Instance;
     public enum MenuState { STARTSCREEN, LOGIN, SIGNUP}
     private MenuState currentMenuState;
+    private Animator animator;
 
     //1
     [Header("Screen States")]
@@ -20,44 +21,47 @@ public class LoginCanvasController : MonoBehaviour
 
     void Start()
     {
+        animator = GetComponent<Animator>();
         Instance = this;
         currentMenuState = MenuState.STARTSCREEN;
         allScreens = new List<GameObject> { startScreen, loginScreen, signupScreen};
-        UpdateActiveScreen((MenuState)0);
+        DisableScreens();
     }
 
     public void UpdateActiveScreen(MenuState _newState)
     {
+        SetAnimatorValues((int)_newState);
+        return;
+
+    }
+
+    public void DisableScreens()
+    {
         foreach (var item in allScreens)
         {
             item.SetActive(false);
+            Debug.Log("Disabled " + item.name);
         }
-
-        switch (_newState)
-        {
-            case MenuState.STARTSCREEN:
-                startScreen.SetActive(true);
-                break;
-            case MenuState.LOGIN:
-                loginScreen.SetActive(true);
-                break;
-            case MenuState.SIGNUP:
-                signupScreen.SetActive(true);
-                break;
-            default:
-                break;
-        }
-
-        currentMenuState = _newState;
     }
 
     public void UpdateActiveScreen(int _stateNumber)
     {
         UpdateActiveScreen((MenuState)_stateNumber);
+
+    }
+
+    public void FadeOut()
+    {
+        animator.Play("FadeOut");
     }
 
     public void ChangeToBuildScene()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void SetAnimatorValues(int _stateNumber)
+    {
+        animator.SetInteger("MenuState", _stateNumber);
     }
 }
